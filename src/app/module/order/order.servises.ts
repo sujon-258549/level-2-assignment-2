dcdff4768f361034285102c78a38fb498feaad2d
Md Sujon Mia <sujon258549@gmail.com>
2025-02-05 02:46:53 +0600

    modified settings.json and order.servises.ts

--- src/app/module/order/order.servises.ts
+++ src/app/module/order/order.servises.ts
@@ -7,20 +7,18 @@ const createOrder = async (order: TOrder) => {
   const car = order?.car;
   // Find the car by ID
   const inStockCar = await CarModel.findById(car);
+  const isInstok = inStockCar?.quantity + 1 > order?.quantity;
+  console.log(inStockCar?.quantity + 1, order?.quantity);
   // Validate car stock
-  console.log(inStockCar?.quantity, order?.quantity);
-  if (
-    !inStockCar ||
-    inStockCar?.inStock === false ||
-    inStockCar?.quantity > order?.quantity
-  ) {
+  console.log(inStockCar?.quantity + 1 >= order?.quantity);
+  if (!inStockCar || inStockCar?.inStock === false || !isInstok) {
     throw new Error('Car stock is unavailable.');
   }
 
   // Create the order
-  const result = await OrderModel.create(order);
+  //   const result = await OrderModel.create(order);
 
-  return result;
+  //   return result;
 };
 
 export const orderServices = {
