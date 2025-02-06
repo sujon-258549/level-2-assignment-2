0bd6249940c5e4e07651a318f2c3b774d32fb063
Md Sujon Mia <sujon258549@gmail.com>
2025-02-06 19:35:18 +0600

    auto-commit

--- src/app/module/order/order.servises.ts
+++ src/app/module/order/order.servises.ts
@@ -1,16 +1,15 @@
 import { CarModel } from '../car/car.model';
 import { TOrder } from './order.interface';
-import { OrderModel } from './order.model';
 
 const createOrder = async (order: TOrder) => {
   console.log(order?.car);
   const car = order?.car;
   // Find the car by ID
   const inStockCar = await CarModel.findById(car);
+  //   @ts-expect-error instok car
   const isInstok = inStockCar?.quantity + 1 > order?.quantity;
-  console.log(inStockCar?.quantity + 1, order?.quantity);
+
   // Validate car stock
-  console.log(inStockCar?.quantity + 1 >= order?.quantity);
   if (!inStockCar || inStockCar?.inStock === false || !isInstok) {
     throw new Error('Car stock is unavailable.');
   }
